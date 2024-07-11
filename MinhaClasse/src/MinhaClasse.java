f115b723abf388f7c1317906e8956cff9dd204cf
Joquebede Oliveira <jotsgama@gmail.com>
2024-07-11 18:26:50 -0300

    #2

--- MinhaClasse/src/MinhaClasse.java
+++ MinhaClasse/src/MinhaClasse.java
@@ -1,6 +1,7 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-         System.out.println("Hllou, vamos começar!!");
+         System.out.println("Hellou, vamos começar!!");
+         
     }
 }
