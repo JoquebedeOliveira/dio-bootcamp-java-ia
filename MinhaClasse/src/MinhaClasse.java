4c1f085a0390216063bcd666d7cfa378e0bba567
Joquebede Oliveira <jotsgama@gmail.com>
2024-07-16 11:11:44 -0300

    CommitSobreMétodo_Java

--- MinhaClasse/src/MinhaClasse.java
+++ MinhaClasse/src/MinhaClasse.java
@@ -2,6 +2,17 @@ public class MinhaClasse {
 
     public static void main(String[] args) {
          System.out.println("Hellou, vamos começar!!");
-         
+           
+         String primeiroNome = "Joquebede";
+         String segundoNome = "Oliveira";
+
+         String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        
+         System.out.println(nomeCompleto);
+
     }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return  "Resultado do método " + primeiroNome.concat(" ").concat( segundoNome);
+    }
+
 }
