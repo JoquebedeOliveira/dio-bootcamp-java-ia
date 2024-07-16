7d55d82a4a662a2a1e9bbf927e46b092d9b2d09b
Joquebede Oliveira <jotsgama@gmail.com>
2024-07-16 11:27:38 -0300

    CommitBoletimEstundantil

---
+++ MinhaClasse/src/BoletimEstudantil.java
@@ -0,0 +1,12 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediFinal = 4;
+        if (mediFinal<6)
+            System.out.println("REPROVADO");
+        else if (mediFinal==6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+    
+}
